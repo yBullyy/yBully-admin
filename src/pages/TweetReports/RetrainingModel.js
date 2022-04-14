87fb452158fe77f714516a272f1755c9a4ed99ca
shubh <shahshubh1010@gmail.com>
2022-04-14 19:02:59 +0530

    added retraining model page

---
+++ src/pages/TweetReports/RetrainingModel.js
@@ -0,0 +1,168 @@
+import React, { useEffect, useState } from "react";
+import { Row, Col, Table, Button, Card } from "react-bootstrap";
+import ReactLoading from 'react-loading';
+
+import Aux from "../../hoc/_Aux";
+import DEMO from "../../store/constant";
+
+import { useCollection } from "react-firebase-hooks/firestore";
+import { batchDeleteApprovedTweets, deleteSingleAprrovedTweet, getApprovedTweets, updateUserStats } from "../../helpers/firestore";
+import { toast } from "react-toastify";
+
+const RetrainingModel = () => {
+
+    const [allReportsValue] = useCollection(getApprovedTweets(), { snapshotListenOptions: { includeMetadataChanges: true } });
+
+    const [approvedReports, setApprovedReports] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        let newApprovedReports = [];
+
+        allReportsValue && allReportsValue.docs.forEach(doc => {
+            newApprovedReports.push(doc.data());
+        });
+
+        setApprovedReports(newApprovedReports);
+    }, [allReportsValue]);
+
+    const handleReject = (index, data, setData) => {
+        try {
+            const newData = [...data];
+            let rejectedTweet = newData.splice(index, 1);
+            setData(newData);
+            console.log(rejectedTweet);
+            deleteSingleAprrovedTweet(rejectedTweet[0].tweetId);
+            toast.success('Tweet Rejected Successfully');
+        } catch (error) {
+            toast.error('Error while rejecting tweet');
+            console.log(error);
+        }
+    }
+
+    const openInNewTab = (url) => {
+        const newWindow = window.open(url, '_blank', 'noopener,noreferrer');
+        if (newWindow) {
+            newWindow.opener = null;
+        }
+    }
+
+    const onRetrainClick = () => {
+        setIsLoading(true);
+        try {
+            // update stats
+            let userTweetMap = {};
+            approvedReports.forEach((tweet) => {
+                tweet.reportedBy.forEach((userId) => {
+                    if (!userTweetMap[userId]) {
+                        userTweetMap[userId] = [];
+                    }
+                    userTweetMap[userId].push(tweet);
+                });
+            });
+            Object.keys(userTweetMap).forEach((userId) => {
+                const userTweetsCount = userTweetMap[userId].length;
+                console.log(`${userId} has ${userTweetsCount} tweets`);
+                updateUserStatsHandler(userId, userTweetsCount);
+            });
+
+            // clear approved tweets collection
+            const approvedTweetIds = approvedReports.map(approvedTweet => approvedTweet.tweetId);
+            batchDeleteApprovedTweets(approvedTweetIds);
+
+            // send data to retraining model
+
+        } catch (error) {
+            toast.error('Some error occured !!');
+            console.log(error);
+        }
+        setIsLoading(false);
+    }
+
+    const updateUserStatsHandler = async (userId, approvedCount) => {
+        await updateUserStats(userId, 0, 0, 0, approvedCount);
+    }
+
+    const getTable = (data, setData) => {
+        return (
+            <Aux>
+                <Table responsive hover>
+                    <thead style={{ textAlign: 'center' }} >
+                        <tr>
+                            <th>Sr No.</th>
+                            <th>Tweet</th>
+                            <th>Correct Label</th>
+                            <th>Report Count</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            data.map((item, index) => {
+                                return (
+                                    <tr key={item.tweetId} className="unread">
+                                        <td><h6 className="mb-1">{index + 1}</h6></td>
+                                        <td onClick={() => openInNewTab(item.tweetUrl)} style={{ cursor: 'pointer' }} ><h6 className="mb-1">{item.tweetText}</h6></td>
+                                        <td><h6 className="mb-1">{item.correctLabel}</h6></td>
+                                        <td><h6 className="mb-1">{item.reportCount}</h6></td>
+                                        <td>
+                                            <a
+                                                onClick={() => handleReject(index, data, setData)}
+                                                href={DEMO.BLANK_LINK}
+                                                className="label theme-bg3 text-white f-12"
+                                            >
+                                                Reject
+                                            </a>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </Table>
+            </Aux>
+        );
+    };
+
+    return (
+        <Aux>
+            <Row>
+                <Col
+                    className='m-b-30'
+                    style={{
+                        height: '525px',
+                        overflowY: 'scroll',
+                        // background: '#fff', 
+                        // padding: '20px', 
+                        // borderRadius: '10px', 
+                        // boxShadow: '-5px 0px 5px #ccc' 
+                    }}
+                >
+                    <Card>
+                        <Card.Header style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <Card.Title as="h5">
+                                Approved Tweets For Retraining
+                            </Card.Title>
+                        </Card.Header>
+                        <Card.Body>
+                            {getTable(approvedReports, setApprovedReports)}
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <div style={{ display: 'flex', flexDirection: 'row-reverse' }} >
+                <Button variant="primary" onClick={onRetrainClick} >
+                    {
+                        isLoading
+                            ? <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} >
+                                <ReactLoading type="spinningBubbles" color="#fff" height={20} width={20} />
+                            </div>
+                            : "Retrain Model"
+                    }
+                </Button>
+            </div>
+        </Aux>
+    );
+}
+
+export default RetrainingModel;
